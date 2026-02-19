084b6344866770dd1cc91d9040baa7274e04192d
Srushti2225 <srushtikatkar69@gmail.com>
2026-02-19 12:27:31 +0530

    Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,54 @@
 import React from 'react'
 import logo from './../assets/logo.png'
+import { HiHome, 
+      HiMagnifyingGlass,
+      HiStar,
+      HiPlayCircle,
+      HiTv} from 'react-icons/hi2'  
+import {HiPlus, HiDotsVertical} from 'react-icons/hi'
+import HeaderItems from './HeaderItems'
 
 const Header = () => {
+  const menu =[
+    {
+      name : 'HOME',
+      icon : HiHome
+    },
+    {
+      name : 'SEARCH',
+      icon : HiMagnifyingGlass
+    },
+    {
+      name : 'WATCH LIST',
+      icon : HiPlus
+    },
+    {
+      name : 'ORIGINALS',
+      icon : HiStar
+    },
+    {
+      name : 'MOVIES',
+      icon : HiPlayCircle
+    },
+    {
+      name : 'SERIES',
+      icon : HiTv
+    },
+
+  ]
   return (
-    <div>
+    <div className='flex justify-between items-center p-5'>
+    <div className='flex items-center gap-8'>
         <img src={logo} className='w-[80px] md:w-[115px] object-cover'/>
+        {menu.map((item)=>(
+          <HeaderItems name={item.name} Icon={item.icon}/>
+        ))}
+        
+    </div>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhMHSJ38KkFKc0NUiMjB52zle58YBCOb4Fqg&s" alt="profile" className='w-[60px] h-[60px] rounded-full'/>
     </div>
   )
 }
 
 export default Header
+
---
+++ src/components/HeaderItems.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeaderItems = (props) => {
+  return (
+    <div className="relative group text-white flex items-center gap-3 font-semibold text-[15px] cursor-pointer">
+      
+      <props.Icon className="text-xl" />
+      <h2>{props.name}</h2>
+
+      {/* Animated underline */}
+      <span className="absolute left-0 -bottom-2 h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
+
+    </div>
+  )
+}
+
+export default HeaderItems
