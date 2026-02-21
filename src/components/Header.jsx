35a16b303e17f83864d43fe9bab101419266180c
Srushti2225 <srushtikatkar69@gmail.com>
2026-02-21 14:21:34 +0530

    Responsive navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './../assets/logo.png'
 import { HiHome, 
       HiMagnifyingGlass,
@@ -9,6 +9,7 @@ import {HiPlus, HiDotsVertical} from 'react-icons/hi'
 import HeaderItems from './HeaderItems'
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
   const menu =[
     {
       name : 'HOME',
@@ -38,14 +39,28 @@ const Header = () => {
   ]
   return (
     <div className='flex justify-between items-center p-5'>
-    <div className='flex items-center gap-8'>
+    <div className='flex items-center gap-10'>
         <img src={logo} className='w-[80px] md:w-[115px] object-cover'/>
-        {menu.map((item)=>(
+        <div className='hidden md:flex gap-8'>
+          {menu.map((item)=>(
           <HeaderItems name={item.name} Icon={item.icon}/>
         ))}
-        
+        </div>
+        <div className='flex md:hidden gap-8'>
+          {menu.map((item, index)=>index < 3 &&(
+          <HeaderItems name={''} Icon={item.icon}/>
+        ))}
+        </div>
+        <div className='md:hidden' onClick={()=>setToggle(!toggle)}>
+          <HeaderItems name={''} Icon={HiDotsVertical}/>
+          {toggle?<div className='absolute mt-3 bg-[#121212] border border-gray-700 p-3 px-5 py-4'>
+            {menu.map((item, index)=>index >= 3 &&(
+          <HeaderItems name={item.name} Icon={item.icon}/>
+        ))}
+          </div> : null}
+        </div>             
     </div>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhMHSJ38KkFKc0NUiMjB52zle58YBCOb4Fqg&s" alt="profile" className='w-[60px] h-[60px] rounded-full'/>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhMHSJ38KkFKc0NUiMjB52zle58YBCOb4Fqg&s" alt="profile" className='w-[50px] h-[50px] rounded-full'/>
     </div>
   )
 }
--- src/components/HeaderItems.jsx
+++ src/components/HeaderItems.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 
 const HeaderItems = (props) => {
   return (
-    <div className="relative group text-white flex items-center gap-3 font-semibold text-[15px] cursor-pointer">
+    <div className="relative group text-white flex items-center gap-3 font-semibold text-[15px] cursor-pointer mb-3">
       
       <props.Icon className="text-xl" />
       <h2>{props.name}</h2>
-
-      {/* Animated underline */}
       <span className="absolute left-0 -bottom-2 h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
 
     </div>
