1e7d4d537a5c861aa65e5719e67db0c1c27ec3d7
Srushti2225 <srushtikatkar69@gmail.com>
2026-02-24 16:20:31 +0530

    checking

--- src/components/ProductionCards.jsx
+++ src/components/ProductionCards.jsx
@@ -42,8 +42,9 @@ const ProductionCards = () => {
     
   return (
     
-    <div className='flex gap-2 md:gap-5 p-2 px-5 md:px-16 py-12'> {ProductionCardList.map((item)=>( 
-    <div className='border-2 border-gray-600 rounded-lg hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer relative shadow-2xl shadow-black '>
+    <div className='flex gap-2 md:gap-5 p-2 px-5 md:px-16 md:py-12'> {ProductionCardList.map((item)=>( 
+    <div key={item.id} className='border-2 border-gray-600 rounded-lg hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer relative shadow-2xl shadow-black '>
+        
          <video src={item.video} autoPlay loop playsInline muted className='absolute z-0 top-0 rounded-md opacity-0 hover:opacity-50'/> 
          <img src={item.image} className='w-full z-1 opacity-100' /> </div> ))} </div>
   )
